2548e6a9d10e3b3d2764a9021d17c3f6659385cd
iamfirmansyah <imamfirmansyah724@gmail.com>
2022-05-06 23:20:37 +0700

    generate random id

--- src/GenerateID.php
+++ src/GenerateID.php
@@ -30,4 +30,31 @@ class GenerateID
             }
         }
     }
+
+    public static function random($size, $withSpecialCharacters = false)
+    {
+        $code = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        $code .= "abcdefghijklmnopqrstuvwxyz";
+        $code .= "0123456789";
+
+        $token = self::generate($code, $size, $withSpecialCharacters);
+
+        return $token;
+    }
+
+    private static function generate($characters, $size, $withSpecialCharacters = false)
+    {
+        if ($withSpecialCharacters) {
+            $characters .= '!@#$%^&*()';
+        }
+
+        $token = '';
+        $max = strlen($characters);
+        for ($i = 0; $i < $size; $i++) {
+            $token .= $characters[random_int(0, $max - 1)];
+        }
+
+        return $token;
+    }
+
 }
